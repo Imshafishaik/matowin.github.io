2a46c259f0cdf1397fde56dabc5c4f2938409df4
shaik shafi <shaik.shafi@careers360.in>
2024-08-25 01:33:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import AboutUs from './components/AboutUs';
+import Header from './components/Header';
+import Home from './components/Home';
+import OurInfrastructure from './components/OurInfrastructure';
+import Products from './components/Products';
+import Services from './components/Services';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home />
+      <div className='our_infra'>
+      <OurInfrastructure />
+      </div>
+      <AboutUs />
+      <Services />
+      <Products />
     </div>
   );
 }
